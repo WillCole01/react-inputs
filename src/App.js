dfd32167ce03d9ae9b25eb0b4d32b9ed08b60720
willcole1987 <will.david.cole@gmail.com>
2023-02-25 21:57:38 +0000

    state management classes (WordParser and Lister) moved to parent to avoid reinstantiation on inputlist re-render

--- src/App.js
+++ src/App.js
@@ -6,16 +6,23 @@ import { useState, React } from "react";
 import Button from "./components/Button";
 import TextArea from "./components/TextArea";
 
+// autocomplete and translation helpers
+import  add, {WordParser,Lister} from '../utils/CalcAutocompletion.js';
+import '../utils/Globals.js';
+
 export default function App() {
   const [inputs, setInputs] = useState([{ key: 1, inputText: "" }]);
   const [id, setId] = useState(2);
   const [activeInput, setActiveInput] = useState(inputs[0]);
 
+  // autocomplete and translations helper classes
+  const w = new WordParser();
+  const l = new Lister(w, componentArguments, grammars );
+  const [avtiveInputList, setActiveInputList] = useState(l.getInitialList()); // we're only interested in the active input at any one time -> just one state var!
+
+  // INPUTS properties => change / activate 
   const changeActive = (input) =>
   {
-    console.log('runs')
-    console.log(input)
-
     const i = inputs.findIndex(x => x.key === input);
     setActiveInput(inputs[i]);
   };
@@ -23,11 +30,13 @@ export default function App() {
   const changeInput = (input, text) => {
     const newInputs = [...inputs];
     const i = newInputs.findIndex((a) => a.key === input.key);
-    newInputs[i].inputText = text;
+    newInputs[i].inputText = text; // todo = here -> should use the autocomplete / translation object to determine new text (enter, dot, bracket)
     Object.freeze(newInputs);
     setInputs(newInputs);
   };
 
+
+  // Inputs => add / remove
   const addInput = () => {
     setId(id + 1);
     const newInput = { key: id, inputText: "" };
--- src/components/InputList.js
+++ src/components/InputList.js
@@ -1,16 +1,9 @@
 import Input from "./Input.js";
-import  add, {WordParser,Lister} from '../utils/CalcAutocompletion.js';
-import '../utils/Globals.js';
 // import ScriptTag from 'react-script-tag';
 
 
 //handleActive={handleActive} handleActive
 const InputList = ({ inputs, changeInput }) => {
-
-    const w = new WordParser();
-    const l = new Lister(w, componentArguments, grammars );
-    const initialList = l.getInitialList();
-
 
   const renderInput = (input, initialList, changeInput) => {
 
